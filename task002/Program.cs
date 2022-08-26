73672194de1bd7cbbc556f2e0b0f1037a20348d9
dmitriy <dmitbely@protonmail.ch>
2022-08-26 13:52:48 +0300

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,20 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Является ли число квадратом другого? Проверим. Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Теперь введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (((number1 * number1) != number2) && ((number2 * number2) != number1))
+{
+    Console.WriteLine($"Числа {number1} и {number2} не являются корнями друг друга");
+}
+
+else if ((number1 * number1) == number2)
+{
+    Console.WriteLine($"{number2} это квадрат числа {number1}");
+}
+else if ((number2 * number2) == number1)
+{
+    Console.WriteLine($"{number1} это квадрат числа {number2}");
+}
